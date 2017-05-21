bb7658a6f3e3a9632230ab6ac387376179265b68
NCssb64 <wdyhssm@gmail.com>
2017-05-21 15:32:57 -0400

    making smashplots have a flag to suppresss; refactoring code for simplicity

--- smashplots.py
+++ smashplots.py
@@ -12,7 +12,7 @@ import smasharchive
 
 labels = ['pools', 'bracket', 'top32', 'top16', 'top8', 'top4', 'grands', 'reset']
 
-def make_single_plot(temp_char_map, temp_char_name, pltname):
+def make_single_plot(temp_char_map, temp_char_name, pltname, print_plots=False):
     # plt.figure(num=None, figsize=(8, 5), dpi=100, facecolor='w', edgecolor='k')
     fig, ax = plt.subplots()
     
@@ -26,7 +26,8 @@ def make_single_plot(temp_char_map, temp_char_name, pltname):
     plt.xlabel('Stage of tournament')
     plt.ylabel('Ratio of total character usage that stage')
 
-    use_labels = labels[0:len(temp_char_map[0])]
+    end_idx = min( len(temp_char_map[0]), len(labels) )
+    use_labels = labels[0:end_idx]
     plt.xticks(range(len(use_labels)), use_labels, rotation='vertical')
 
     ax.legend(temp_char_name, loc='center right', bbox_to_anchor=(1.25, 0.50))
@@ -34,13 +35,14 @@ def make_single_plot(temp_char_map, temp_char_name, pltname):
     #plt.tight_layout()
     plt.gcf().subplots_adjust(wspace=2, hspace=2, right=0.825, bottom=0.25)
     #plt.gcf().subplots_adjust(right=None, left=None, top=None, bottom=None)
+
+    plt.show()
+    if print_plots == True:
+        plt.savefig('../smasharticle/figures/{msg:>0}.eps'.format(msg=pltname), format='eps')
+        plt.savefig('../smasharticle/figures/{msg:>0}.png'.format(msg=pltname))
+        plt.close(fig)
     
-    #plt.show()
-    plt.savefig('../smasharticle/figures/{msg:>0}.eps'.format(msg=pltname), format='eps')
-    plt.savefig('../smasharticle/figures/{msg:>0}.png'.format(msg=pltname))
-    plt.close(fig)
-    
-def plot_char_use(db, plotnameroot):
+def plot_char_use(db, plotnameroot, plot_flag=False):
     # db = smasharchive.smashdb()
     levels = [
         [0,0],
@@ -65,19 +67,26 @@ def plot_char_use(db, plotnameroot):
             char_use_map[db.muchar_tots[j][0]][which_level] = db.muchar_tots[j][1]
             games_at_stage[which_level] += db.muchar_tots[j][1]
 
-    # Upper tiers
-    nametag = '-hightier'
     splitting_point = 0.10
-    temp_char_map = []
-    temp_char_name = []
-    for key in char_use_map.keys():
-        data = np.divide( char_use_map[key] , games_at_stage )
-        if max(data) < splitting_point:
-            continue
-        temp_char_map.append(data)
-        temp_char_name.append(key)
-
-    make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag )
+    nametags = ['-hightier', '-lowtier', '-fullcast']
+    intervals = [ [0, splitting_point],
+                  [splitting_point, 1],
+                  [0, 1]
+                ]
+    # ALL PLOTS
+    for which_plot in range(len(nametags)):
+        nametag = nametags[which_plot]
+        temp_char_map = []
+        temp_char_name = []
+        for key in char_use_map.keys():
+            data = np.divide( char_use_map[key] , games_at_stage )
+            a = intervals[which_plot][0]
+            b = intervals[which_plot][1]
+            if max(data) < b and max(data) >= a:
+                continue
+            temp_char_map.append(data)
+            temp_char_name.append(key)
+        make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag, plot_flag)
 
     # Low tiers
     nametag = '-lowtier'
@@ -89,7 +98,7 @@ def plot_char_use(db, plotnameroot):
             continue
         temp_char_map.append(data)
         temp_char_name.append(key)
-    make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag )
+    make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag, plot_flag)
 
     # Full cast
     nametag = '-fullcast'
@@ -100,4 +109,4 @@ def plot_char_use(db, plotnameroot):
         temp_char_map.append(data)
         temp_char_name.append(key)
 
-    make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag )
+    make_single_plot(temp_char_map, temp_char_name, plotnameroot+nametag, plot_flag)
